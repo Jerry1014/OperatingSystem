878bd0f1d8baeec1ee656cba150101cb7520fc39
Jerry <757320383@qq.com>
2019-06-23 21:33:46 +0800

    代码风格修正

--- curriculum design/Unitest/KernelTest.py
+++ curriculum design/Unitest/KernelTest.py
@@ -1,5 +1,6 @@
 import os
 import unittest
+
 from VirtualDiskVisualization import visualization
 
 
@@ -16,15 +17,15 @@ class KernelTest(unittest.TestCase):
     #     visualization('my_virtual_hard_disk')
     #
     def test_for_add_file_or_directory_from_init(self):
-            if os.path.exists('my_virtual_hard_disk'):
-                os.remove('my_virtual_hard_disk')
+        if os.path.exists('my_virtual_hard_disk'):
+            os.remove('my_virtual_hard_disk')
 
-            from Kernel import kernel
-            kernel.add_directory_or_file('/etc/')
-            kernel.add_directory_or_file('/etc/psw/')
-            kernel.add_directory_or_file('/etc/psw/psw.txt',b'12345678')
-            kernel.shut_down()
-            visualization('my_virtual_hard_disk')
+        from Kernel import kernel
+        kernel.add_directory_or_file('/etc/')
+        kernel.add_directory_or_file('/etc/psw/')
+        kernel.add_directory_or_file('/etc/psw/psw.txt', b'12345678')
+        kernel.shut_down()
+        visualization('my_virtual_hard_disk')
 
 
 if __name__ == '__main__':
